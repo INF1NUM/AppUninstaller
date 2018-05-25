aac150db9951308c2750714cf17b6e0e4de5deff
Антон <infinum@live.ru>
2018-05-25 12:04:17 +0300

    Добавьте файлы проекта.

---
+++ AppUninstaller/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace AppUninstaller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonServer = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeaderTime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderMessage = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listViewPackage = new System.Windows.Forms.ListView();
+            this.columnHeaderPackage = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderPath = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.comboBoxDevices = new System.Windows.Forms.ComboBox();
+            this.backgroundWorkerStartServer = new System.ComponentModel.BackgroundWorker();
+            this.toolStripTextBoxFilter = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripLabelFilter = new System.Windows.Forms.ToolStripLabel();
+            this.panel1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonServer
+            // 
+            this.buttonServer.BackColor = System.Drawing.Color.Red;
+            this.buttonServer.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonServer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonServer.ForeColor = System.Drawing.Color.White;
+            this.buttonServer.Location = new System.Drawing.Point(17, 15);
+            this.buttonServer.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonServer.Name = "buttonServer";
+            this.buttonServer.Size = new System.Drawing.Size(118, 26);
+            this.buttonServer.TabIndex = 0;
+            this.buttonServer.Text = "Start server";
+            this.buttonServer.UseVisualStyleBackColor = false;
+            this.buttonServer.Click += new System.EventHandler(this.buttonServer_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderTime,
+            this.columnHeaderMessage});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.listView1.Location = new System.Drawing.Point(17, 404);
+            this.listView1.Margin = new System.Windows.Forms.Padding(4);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(732, 134);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeaderTime
+            // 
+            this.columnHeaderTime.Text = "Time";
+            this.columnHeaderTime.Width = 163;
+            // 
+            // columnHeaderMessage
+            // 
+            this.columnHeaderMessage.Text = "Message";
+            this.columnHeaderMessage.Width = 380;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.listViewPackage);
+            this.panel1.Controls.Add(this.toolStrip1);
+            this.panel1.Location = new System.Drawing.Point(17, 59);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(733, 337);
+            this.panel1.TabIndex = 2;
+            // 
+            // listViewPackage
+            // 
+            this.listViewPackage.CheckBoxes = true;
+            this.listViewPackage.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderPackage,
+            this.columnHeaderPath});
+            this.listViewPackage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listViewPackage.FullRowSelect = true;
+            this.listViewPackage.GridLines = true;
+            this.listViewPackage.Location = new System.Drawing.Point(0, 27);
+            this.listViewPackage.Margin = new System.Windows.Forms.Padding(4);
+            this.listViewPackage.Name = "listViewPackage";
+            this.listViewPackage.Size = new System.Drawing.Size(733, 310);
+            this.listViewPackage.TabIndex = 1;
+            this.listViewPackage.UseCompatibleStateImageBehavior = false;
+            this.listViewPackage.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeaderPackage
+            // 
+            this.columnHeaderPackage.Text = "Package";
+            this.columnHeaderPackage.Width = 150;
+            // 
+            // columnHeaderPath
+            // 
+            this.columnHeaderPath.Text = "Path";
+            this.columnHeaderPath.Width = 200;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripSeparator1,
+            this.toolStripButton2,
+            this.toolStripTextBoxFilter,
+            this.toolStripLabelFilter});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(733, 27);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(82, 24);
+            this.toolStripButton1.Text = "Update";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 27);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(90, 24);
+            this.toolStripButton2.Text = "Uninstall";
+            // 
+            // comboBoxDevices
+            // 
+            this.comboBoxDevices.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxDevices.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxDevices.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.comboBoxDevices.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBoxDevices.FormattingEnabled = true;
+            this.comboBoxDevices.Location = new System.Drawing.Point(502, 15);
+            this.comboBoxDevices.Name = "comboBoxDevices";
+            this.comboBoxDevices.Size = new System.Drawing.Size(248, 26);
+            this.comboBoxDevices.TabIndex = 3;
+            // 
+            // backgroundWorkerStartServer
+            // 
+            this.backgroundWorkerStartServer.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorkerStartServer_DoWork);
+            this.backgroundWorkerStartServer.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorkerStartServer_RunWorkerCompleted);
+            // 
+            // toolStripTextBoxFilter
+            // 
+            this.toolStripTextBoxFilter.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripTextBoxFilter.Name = "toolStripTextBoxFilter";
+            this.toolStripTextBoxFilter.Size = new System.Drawing.Size(200, 27);
+            // 
+            // toolStripLabelFilter
+            // 
+            this.toolStripLabelFilter.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripLabelFilter.Name = "toolStripLabelFilter";
+            this.toolStripLabelFilter.Size = new System.Drawing.Size(42, 24);
+            this.toolStripLabelFilter.Text = "Filter";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(767, 554);
+            this.Controls.Add(this.comboBoxDevices);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.buttonServer);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonServer;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeaderTime;
+        private System.Windows.Forms.ColumnHeader columnHeaderMessage;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ListView listViewPackage;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ColumnHeader columnHeaderPackage;
+        private System.Windows.Forms.ColumnHeader columnHeaderPath;
+        private System.Windows.Forms.ComboBox comboBoxDevices;
+        private System.ComponentModel.BackgroundWorker backgroundWorkerStartServer;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBoxFilter;
+        private System.Windows.Forms.ToolStripLabel toolStripLabelFilter;
+    }
+}
+
---
+++ AppUninstaller/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpAdbClient;
+using SharpAdbClient.DeviceCommands;
+
+namespace AppUninstaller
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            devices = new BindingList<DeviceData>();
+            comboBoxDevices.DataSource = devices;
+            comboBoxDevices.DisplayMember = "Serial";
+        }
+
+        private BindingList<DeviceData> devices;
+        private bool adbServerIsRunning = false;
+        private DeviceMonitor deviceMonitor;
+        private bool ServerIsRunning
+        {
+            get { return adbServerIsRunning; }
+            set
+            {
+                adbServerIsRunning = value;
+                if (value)
+                {
+                    buttonServer.BackColor = Color.Green;
+                    buttonServer.Text = "Stop";
+                }
+                else
+                {
+                    buttonServer.BackColor = Color.Red;
+                    buttonServer.Text = "Start";
+                }
+            }
+        }
+        
+        private void backgroundWorkerStartServer_DoWork(object sender, DoWorkEventArgs e)
+        {
+            e.Result = AdbServer.Instance.StartServer(Application.StartupPath + @"\tools\adb.exe", false);
+        }
+
+        private void backgroundWorkerStartServer_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            var result = (StartServerResult)e.Result;
+            if (AdbServer.Instance.GetStatus().IsRunning)
+            {
+                this.ServerIsRunning = true;
+                deviceMonitor = new DeviceMonitor(new AdbSocket(new IPEndPoint(IPAddress.Loopback, AdbClient.AdbServerPort)));
+                deviceMonitor.DeviceConnected += OnDeviceConnected;
+                deviceMonitor.DeviceDisconnected += OnDeviceDisconnected;
+                deviceMonitor.Start();
+                buttonServer.Enabled = true;
+                WriteLog($"Server started with result: {result.ToString()}");
+                WriteLog($"ADB version: {AdbClient.Instance.GetAdbVersion()}");
+            }
+            else
+                WriteLog($"Server NOT started with result: {result.ToString()}");
+        }
+
+        private void StopServer()
+        {
+            deviceMonitor.Dispose();
+            AdbClient.Instance.KillAdb();
+            ServerIsRunning = false;
+            devices.Clear();
+            listViewPackage.Items.Clear();
+        }
+
+        private void WriteLog(string message)
+        {
+            if (listView1.InvokeRequired)
+            {
+                listView1.BeginInvoke(new MethodInvoker(() => listView1.Items.Add(DateTime.Now.ToString(@"HH:mm:ss.ffff")).SubItems.Add(message)));
+                listView1.BeginInvoke(new MethodInvoker(() => listView1.EnsureVisible(listView1.Items.Count - 1)));
+            }
+            else
+            {
+                listView1.Items.Add(DateTime.Now.ToString(@"HH:mm:ss.ffff")).SubItems.Add(message);
+                listView1.EnsureVisible(listView1.Items.Count - 1);
+            }
+        }
+        
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //if (adbServer != null && adbServer.GetStatus().IsRunning)
+            //{
+            //    var result = MessageBox.Show("ADB server is running. Do you want to kill server and exit", "ADB Uninstaller", MessageBoxButtons.YesNoCancel);
+            //    if (result == DialogResult.Yes)
+            //        AdbClient.Instance.KillAdb();
+            //    else
+            //    {
+            //        if (result == DialogResult.Cancel)
+            //        {
+            //            e.Cancel = true;
+            //        }
+            //    }
+            //}
+        }
+
+        private void buttonServer_Click(object sender, EventArgs e)
+        {
+            if (adbServerIsRunning)
+            {
+                StopServer();
+                WriteLog("Server stopped.");
+            }
+            else
+            {
+                buttonServer.Enabled = false;
+                buttonServer.Text = "Starting...";
+                backgroundWorkerStartServer.RunWorkerAsync();
+            }
+
+            //if (adbServer == null)
+            //{
+            //    WriteLog("Starting daemon...");
+            //    adbServer = StartServer(Application.StartupPath + @"\tools\adb.exe");
+            //    WriteLog("ADB version: " + adbServer.GetStatus().Version.ToString());
+            //    // подписка подключение и отключение устройств
+            //    var monitor = new DeviceMonitor(new AdbSocket(new IPEndPoint(IPAddress.Loopback, AdbClient.AdbServerPort)));
+            //    monitor.DeviceConnected += OnDeviceConnected;
+            //    monitor.DeviceDisconnected += OnDeviceDisconnected;
+            //    monitor.Start();
+            //    buttonServer.Text = "Stop server";
+            //    buttonServer.BackColor = Color.Green;
+            //}
+            //else
+            //{
+
+            //    if (adbServer.GetStatus().IsRunning)
+            //    {
+            //        AdbClient.Instance.KillAdb();
+            //        WriteLog("Server stopped.");
+            //        buttonServer.Text = "Start server";
+            //        buttonServer.BackColor = Color.Red;
+            //    }
+            //    else
+            //    {
+            //        WriteLog("Starting daemon...");
+            //        adbServer = new AdbServer();
+            //        var result = adbServer.StartServer(Application.StartupPath + @"\tools\adb.exe", false);
+            //        WriteLog("Server started.");
+            //        WriteLog("ADB version: " + adbServer.GetStatus().Version.ToString());
+            //        buttonServer.Text = "Stop server";
+            //        buttonServer.BackColor = Color.Green;
+
+            //        // подписка подключение и отключение устройств
+            //        var monitor = new DeviceMonitor(new AdbSocket(new IPEndPoint(IPAddress.Loopback, AdbClient.AdbServerPort)));
+            //        monitor.DeviceConnected += OnDeviceConnected;
+            //        monitor.DeviceDisconnected += OnDeviceDisconnected;
+            //        monitor.Start();
+            //    }
+            //}
+        }
+
+        private void OnDeviceConnected(object sender, DeviceDataEventArgs e)
+        {
+            this.BeginInvoke(new MethodInvoker(() => this.devices.Add(e.Device)));
+            WriteLog($"The device {e.Device.Name} {e.Device.Serial} has connected to this PC");
+        }
+
+        private void OnDeviceDisconnected(object sender, DeviceDataEventArgs e)
+        {
+            this.BeginInvoke(new MethodInvoker(() => this.devices.Remove(e.Device)));
+            WriteLog($"The device {e.Device.Name} {e.Device.Serial} has disconnected to this PC");
+        }
+
+        private void FillPakageList()
+        {
+            if (ServerIsRunning && devices.Count > 0)
+            {
+                var device = (DeviceData)comboBoxDevices.SelectedItem;
+                PackageManager pm = new PackageManager(device);
+                listViewPackage.BeginUpdate();
+                listViewPackage.Items.Clear();
+                foreach (var package in pm.Packages)
+                {
+                    listViewPackage.Items.Add(package.Key).SubItems.Add(package.Value);
+                }
+                listViewPackage.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+                listViewPackage.EndUpdate();
+            }
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            FillPakageList();
+        }
+    }
+}
